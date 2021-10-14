49758a93d8239327cb10688cbb189e6fb298cd51
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-10-15 02:29:42 +0900

    Add more HID keys

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -9,16 +9,16 @@ std::array<struct key_button, 19> key_buttons;
 void set_alphabet_keybard()
 {
   size_t i = 0;
-  key_buttons[i++].set_keys("→", KEY_RightArrow);
-  key_buttons[i++].set_keys("@#/&_", KEY_atmark, KEY_number_sign, KEY_slash, KEY_ampersand, KEY_underscore);
+  key_buttons[i++].set_keys("→", KEY_RIGHT_ARROW);
+  key_buttons[i++].set_keys("@#/&_", KEY_ATMARK, KEY_NUM_SIGN, KEY_FWD_SLASH, KEY_AMPERSAND, KEY_UNDERSCORE);
   key_buttons[i++].set_keys("ABC", KEY_A, KEY_B, KEY_C);
   key_buttons[i++].set_keys("DEF", KEY_D, KEY_E, KEY_F);
-  key_buttons[i++].set_keys("<x", KEY_Backspace);
-  key_buttons[i++].set_keys("←", KEY_LeftArrow);
+  key_buttons[i++].set_keys("<x", KEY_DELETE);
+  key_buttons[i++].set_keys("←", KEY_LEFT_ARROW);
   key_buttons[i++].set_keys("GHI", KEY_G, KEY_H, KEY_I);
   key_buttons[i++].set_keys("JKL", KEY_J, KEY_K, KEY_L);
   key_buttons[i++].set_keys("MNO", KEY_M, KEY_N, KEY_O);
-  key_buttons[i++].set_keys("空白", KEY_Spacebar);
+  key_buttons[i++].set_keys("空白", KEY_SPACEBAR);
   key_buttons[i++].set_keys("123");
   key_buttons[i++].set_keys("PQRS", KEY_P, KEY_Q, KEY_R, KEY_S);
   key_buttons[i++].set_keys("TUV", KEY_T, KEY_U, KEY_V);
@@ -26,8 +26,8 @@ void set_alphabet_keybard()
   key_buttons[i++].set_keys("改行", KEY_ENTER);
   key_buttons[i++].set_keys("(^_^)");
   key_buttons[i++].set_keys("a/A");
-  key_buttons[i++].set_keys("'\"()", KEY_quote, KEY_double_quote, KEY_left_parenthesis, KEY_right_parenthesis);
-  key_buttons[i++].set_keys(".,?!", KEY_period, KEY_comma, KEY_question, KEY_exclamation_mark);
+  key_buttons[i++].set_keys("'\"()", KEY_SGL_QUOTE, KEY_DBL_QUOTE, KEY_LEFT_PAREN, KEY_RIGHT_PAREN);
+  key_buttons[i++].set_keys(".,?!", KEY_DOT, KEY_COMMA, KEY_QUESTION, KEY_EXCL_MARK);
 }
 
 void init_keyboard_layout()
@@ -76,5 +76,5 @@ void input_key_button(struct key_button *key, direction_t dir)
     return;
   }
   draw_input_text(input->text, 270, 150);
-  send_key(input->keycode, input->modifier << 1);
+  send_key(input->keycode, input->modifier);
 }
